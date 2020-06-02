a18d9cc75b732fb2cc88684939f40dc97e28fb16
alan <amdlemos@gmail.com>
2020-06-02 16:58:41 -0300

    modified validations

--- src/models/address.model.ts
+++ src/models/address.model.ts
@@ -1,18 +1,41 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsString, MinLength, MaxLength } from "class-validator";
 
 export class Address {
     @ApiProperty()
-    zipcode: number;
+    zipcode: string;
+
     @ApiProperty()
+    @IsNotEmpty({ message: 'Logradouro é obrigatório.'})
+    @IsString()
+    @MinLength(5, { message: 'Logradouro deve conter no mínimo 5 caracteres.'})
+    @MaxLength(130, { message: 'Logradouro deve conter no máximo 130 caracteres.'})
     street: string;
+
     @ApiProperty()
+    @IsNotEmpty({ message: 'Número é obrigatório.'})
+    @IsString()
+    @MinLength(5, { message: 'Número deve conter no mínimo 1 caracteres.'})
+    @MaxLength(20, { message: 'Número deve conter no máximo 20 caracteres.'})
     number: string;
-    @ApiProperty()
+
+    @ApiProperty()    
+    @IsString()    
+    @MaxLength(50, { message: 'Complemento deve conter no máximo 50 caracteres.'})
     complement: string;
+
     @ApiProperty()
+    @IsNotEmpty({ message: 'Bairro é obrigatório.'})
+    @IsString()    
+    @MinLength(5, { message: 'Bairro deve conter no mínimo 5 caracteres.'})
+    @MaxLength(50, { message: 'Bairro deve conter no máximo 50 caracteres.'})
     neighborhood: string;
+
     @ApiProperty()
+    @IsNotEmpty({ message: 'Cidade é obrigatório.'})
     city_code: string;
+
     @ApiProperty()
+    @IsNotEmpty({ message: 'Estado é obrigatório.'})
     state_code: string;
 }
--- src/models/customer.model.ts
+++ src/models/customer.model.ts
@@ -1,19 +1,37 @@
 import { Address } from "./address.model";
 import { ApiProperty } from "@nestjs/swagger";
+import { MaxLength, MinLength, IsString, IsNotEmpty, IsNumber, IsEmail } from "class-validator";
 
 export class Customer {
     // main data
     _id: string;    
     @ApiProperty()
+    @IsNotEmpty({ message: 'Nome do cliente é obrigatório.'})
+    @IsString()
+    @MinLength(4, { message: 'Nome do cliente deve conter no mínimo 4 caracteres.'})
+    @MaxLength(300, { message: 'Nome do cliente deve conter no máximo 300 caracteres.'})
     name: string;
-    @ApiProperty()
+    
+    @ApiProperty()    
+    @IsString()   
+    @MaxLength(300, { message: 'Nome do cliente deve conter no máximo 300 caracteres.'})
     fantasyName: string;
+    
     @ApiProperty()
+    @IsNotEmpty({ message: 'CNPJ é obrigatorio.'})
+    @IsString()    
     subscription: number;
+
     @ApiProperty()
+    @IsNotEmpty({ message: 'Telefone é obrigatorio.'})
+    @IsString()    
     phone: number;
-    @ApiProperty()
+    
+    @ApiProperty()    
+    @IsEmail()
+    @MaxLength(100, {message: 'Email deve conter no máximo 100 caracteres.'})
     email: string;
+    
     @ApiProperty()    
     address: Address;            
     // contacts: Contact[];    
